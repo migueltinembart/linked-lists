153323718eed70ab50e9d32ea34c23abb4266b61
Tinembart-sudo <miguel.tinembart@gmail.com>
2022-09-28 15:27:15 +0200

    add: linked_list.rb
    read&write data, pointer

---
+++ lib/linked_list.rb
@@ -0,0 +1,18 @@
+##
+# This class represents a standard linked list
+require_relative 'node'
+
+class ClassName
+
+attr_reader = :head, :tail
+
+##
+# initalizes linked list head and tail set as nil
+
+    def initialize
+        @head = nil
+        @tail = nil
+    end
+
+    
+end
