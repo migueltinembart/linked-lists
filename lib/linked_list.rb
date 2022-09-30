ad29e718b085f2954d30530eba3c93af0d368c93
Tinembart-sudo <miguel.tinembart@gmail.com>
2022-09-30 08:21:38 +0200

    feat: elements can be added to linked list
    - #append adds element to end of list
    - #prepend adds element at the beginning of the list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,18 +1,40 @@
 ##
 # This class represents a standard linked list
-require_relative 'node'
 
-class ClassName
 
-attr_reader = :head, :tail
+class LinkedList
+    require_relative 'node'
+    attr_reader :head, :tail
 
-##
-# initalizes linked list head and tail set as nil
+    ##
+    # initalizes linked list head and tail set as nil
 
     def initialize
         @head = nil
         @tail = nil
     end
 
+    ##
+    # #append(value)
+    # adds an element to the end of the list
+    def append(value)
+        new_node = Node.new(value)
+        @head ||= new_node
+        if !@tail
+            @tail = new_node
+        else
+            @tail.next_node = new_node
+        end
+        @tail = new_node
+    end
+
+    ##
+    # adds element add the beginning of the list
+    def prepend(value)
+        next_element = @head
+        @head = Node.new(value, next_element)
+
+    end
     
+
 end
