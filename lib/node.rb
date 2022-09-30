cd6c921bffaa9624b092ffa940a5f81bdb28c103
Tinembart-sudo <miguel.tinembart@gmail.com>
2022-09-30 08:18:20 +0200

    fix: fixed getter and setter declaration

--- lib/node.rb
+++ lib/node.rb
@@ -1,16 +1,12 @@
 ##
 # This class represents a data node which points to another data structure
-
 class Node
+  attr_accessor :data, :next_node
 
-    attr_accessor = :data, :pointer
-
-    ##
-    # initialize node with nil as standard values
-    #
-    def initialize(data = nil, pointer = nil)
-        @data = data
-        @pointer = pointer
-    end
-
+  ##
+  # returns a node object which can hold any element and can point to another value
+  def initialize(data = nil, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
 end
